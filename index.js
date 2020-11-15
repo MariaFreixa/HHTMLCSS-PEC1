b421a2224b15ee26ae8bc1fe3128a23e6968db4f
Maria Freixa Torres <mery.blood@gmail.com>
2020-11-15 10:37:55 +0100

    last update

---
+++ index.js
@@ -0,0 +1,27 @@
+//Index container
+var elementContainer = document.getElementById('container');
+var h3 = document.createElement('h3');
+var h1 = document.createElement('h1');
+var p_container = document.createElement('p');
+
+h3.textContent="Hay un millón de vidas por vivir, ¿entras?";
+h1.textContent="LA MADRIGUERA";
+p_container.textContent="Portal dedicado a ofrecer información sobre diferentes tipos de libros";
+
+elementContainer.appendChild(h3);
+elementContainer.appendChild(h1);
+elementContainer.appendChild(p_container);
+
+//Index Footer
+var elementFooter = document.getElementById('footer');
+var p = document.createElement('p');
+var p_footer = document.createElement('p');
+
+p.textContent="© 2020-2021 Maria Magdalena Freixa Torres.";
+p_footer.textContent="Trabajo realizado para la asignatura Herramientas HTML y CSS. Primer semestre del curso 2020-2021";
+
+elementFooter.appendChild(p);
+elementFooter.appendChild(p_footer);
+
+
+
